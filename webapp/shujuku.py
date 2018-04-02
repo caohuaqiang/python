b9376075520edec985653b307f5a522d7184673a
caohuaqiang <342473195@qq.com>
2018-04-02 14:56:30 +0800

    woshi sb

--- webapp/shujuku.py
+++ webapp/shujuku.py
@@ -4,12 +4,27 @@ dbconfig = {'host': '127.0.0.1',
             'user': 'vsearch',
             'password': '123456',
             'database': 'vsearchlogDB',}
+
 connect = mysql.connector.connect(**dbconfig)
+
 cursor = connect.cursor()
-_SQL = """describe log"""
+
+_SQL = """insert into log
+            (phrase, letters, ip, browser_string, results)
+          values 
+            (%s, %s, %s, %s, %s)"""
+cursor.execute(_SQL, params=('fuck', 'xyz', '127.0.0.1', 'Safari', 'set()'))
+connect.commit()
+
+
+_SQL = """select * from log"""
 cursor.execute(_SQL)
 res = cursor.fetchall()
 print(res)
-print('========================================================================')
+print('================================================================================================================')
 for row in res:
     print(row)
+
+cursor.close()
+connect.close()
+
