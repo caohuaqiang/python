e1ea46a565d7fb67d3c1c7119b1df26abae3636f
caohuaqiang <342473195@qq.com>
2018-04-02 13:46:17 +0800

    'database'

--- webapp/shujuku.py
+++ webapp/shujuku.py
@@ -4,6 +4,12 @@ dbconfig = {'host': '127.0.0.1',
             'user': 'vsearch',
             'password': '123456',
             'database': 'vsearchlogDB',}
-
 connect = mysql.connector.connect(**dbconfig)
 cursor = connect.cursor()
+_SQL = """describe log"""
+cursor.execute(_SQL)
+res = cursor.fetchall()
+print(res)
+print('========================================================================')
+for row in res:
+    print(row)
--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -55,6 +55,3 @@ if __name__ == '__main__':
     # app.run(host='0.0.0.0',port=5000,debug=True)
     # app.run()
     app.run(debug=True)
-
-
-    abcdefg
