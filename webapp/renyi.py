9c276fc5d932ada90c8a5d83b8021c927278dd98
caohuaqiang <342473195@qq.com>
2018-04-03 17:59:03 +0800

    修饰符练习

---
+++ webapp/renyi.py
@@ -0,0 +1,9 @@
+def myfunc2(**kwargs):
+    for k, v in kwargs.items():
+        print(k, v, sep='->', end=' ')
+    if kwargs:
+        print()
+
+
+if __name__ == '__main__':
+    myfunc2(a=1, b=2)
