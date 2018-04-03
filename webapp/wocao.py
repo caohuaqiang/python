07a953b9cd2811f000481da3e92ed22a03d95518
caohuaqiang <342473195@qq.com>
2018-04-03 09:09:30 +0800

    0403

--- webapp/wocao.py
+++
@@ -1,6 +0,0 @@
-if __name__ == '__main__':
-    # app.run(host='0.0.0.0',port=5000,debug=True)
-    # app.run()
-    # app.run(debug=True)
-    with open('chq.log', mode='a') as log:
-        print(123, 'abc', file=log)
